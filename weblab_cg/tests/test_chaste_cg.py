d7c887ab9638887985506ce602320f350d67f5b9
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-12-02 18:33:17 +0000

    Used pytest.parametrize in test_chaste_cg to get a bit more response when testing.

--- weblab_cg/tests/test_chaste_cg.py
+++ weblab_cg/tests/test_chaste_cg.py
@@ -12,46 +12,48 @@ LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
 
+def model_types():
+    return ['Normal']
+
+
+def chaste_models():
+    """ Load all models"""
+    return load_chaste_models(model_types=model_types())
+
+
 class TestChasteCG(object):
     """ Tests weblab_cg against reference models generated with weblab_cg and tested in chaste.
 
     TODO: unit conversion vvia cellmlmanip once implemented"""
     _TIMESTAMP_REGEX = re.compile(r'(//! on .*\n)')
 
-    def model_types(self):
-        return ['Normal']
-
-    @pytest.fixture(scope="class")
-    def chaste_models(self):
-        """ Load all models"""
-        return load_chaste_models(model_types=self.model_types())
-
-    def test_generate_chaste_models(self, tmp_path, chaste_models):
+    @pytest.mark.parametrize(('model'), chaste_models())
+    def test_generate_chaste_model(self, tmp_path, model):
         """ Check generation of Normal models against reference"""
         tmp_path = str(tmp_path)
-        for model in chaste_models:
-            for model_type in model['reference_models'].keys():
-                LOGGER.info('Converting: ' + model_type + ' ' + model['class_name'] + '\n')
-                # Generate chaste code
-                chaste_model = cg.NormalChasteModel(model['model'], model['class_name'], model['model_name_from_file'])
-                chaste_model.generate_chaste_code()
 
-                # Write generated files
-                hhp_gen_file_path = os.path.join(tmp_path, model_type, chaste_model.file_name + ".hpp")
-                cpp_gen_file_path = os.path.join(tmp_path, model_type, chaste_model.file_name + ".cpp")
-                write_file(hhp_gen_file_path, chaste_model.generated_hpp)
-                write_file(cpp_gen_file_path, chaste_model.generated_cpp)
+        for model_type in model['reference_models'].keys():
+            LOGGER.info('Converting: ' + model_type + ' ' + model['class_name'] + '\n')
+            # Generate chaste code
+            chaste_model = cg.NormalChasteModel(model['model'], model['class_name'], model['model_name_from_file'])
+            chaste_model.generate_chaste_code()
 
-                # Load reference files
-                expected_hpp = get_file_lines(model['reference_models'][model_type]['expected_hpp_path'])
-                expected_cpp = get_file_lines(model['reference_models'][model_type]['expected_cpp_path'])
+            # Write generated files
+            hhp_gen_file_path = os.path.join(tmp_path, model_type, chaste_model.file_name + ".hpp")
+            cpp_gen_file_path = os.path.join(tmp_path, model_type, chaste_model.file_name + ".cpp")
+            write_file(hhp_gen_file_path, chaste_model.generated_hpp)
+            write_file(cpp_gen_file_path, chaste_model.generated_cpp)
 
-                # Load generated files
-                generated_hpp = get_file_lines(hhp_gen_file_path)
-                generated_cpp = get_file_lines(cpp_gen_file_path)
+            # Load reference files
+            expected_hpp = get_file_lines(model['reference_models'][model_type]['expected_hpp_path'])
+            expected_cpp = get_file_lines(model['reference_models'][model_type]['expected_cpp_path'])
 
-                assert expected_hpp == generated_hpp
-                assert expected_cpp == generated_cpp
+            # Load generated files
+            generated_hpp = get_file_lines(hhp_gen_file_path)
+            generated_cpp = get_file_lines(cpp_gen_file_path)
+
+            assert expected_hpp == generated_hpp
+            assert expected_cpp == generated_cpp
 
     def test_generate_dymaic_chaste_model(self, tmp_path):
         tmp_path = str(tmp_path)
