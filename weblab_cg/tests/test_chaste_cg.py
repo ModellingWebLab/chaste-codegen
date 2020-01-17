af928044864ad757943de947650ead95801151bd
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2020-01-17 15:33:21 +0000

    manifest update

--- weblab_cg/tests/test_chaste_cg.py
+++ weblab_cg/tests/test_chaste_cg.py
@@ -101,13 +101,13 @@ class TestChasteCG(object):
         model_name = 'hodgkin_huxley_squid_axon_model_1952_modified'
         model_file = os.path.join(cg.DATA_DIR, 'tests', 'cellml', model_name + '.cellml')
         # Convert a cellml file
-#        subprocess.check_output(['translate', model_file], cwd=tmp_path)
-#        # Check output
+        subprocess.check_output(['translate', model_file], cwd=tmp_path)
+        # Check output
         reference = os.path.join(reference_path, 'chaste_reference_models', 'Normal')
-#        compare_file_against_reference(os.path.join(reference, model_name + '.hpp'),
-#                                       os.path.join(tmp_path, model_name + '.hpp'))
-#        compare_file_against_reference(os.path.join(reference, model_name + '.cpp'),
-#                                       os.path.join(tmp_path, model_name + '.cpp'))
+        compare_file_against_reference(os.path.join(reference, model_name + '.hpp'),
+                                       os.path.join(tmp_path, model_name + '.hpp'))
+        compare_file_against_reference(os.path.join(reference, model_name + '.cpp'),
+                                       os.path.join(tmp_path, model_name + '.cpp'))
 
         # Check options: -c -t -o --dynamically-loadable
         outfile = os.path.join(tmp_path, 'output_class.c')
