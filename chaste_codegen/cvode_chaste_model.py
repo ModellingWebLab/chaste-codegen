f7d8f91189896c4abf022d334259f49dc5251c70
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2020-02-07 15:44:16 +0000

    jacobians working, formattig to do

--- chaste_codegen/cvode_chaste_model.py
+++ chaste_codegen/cvode_chaste_model.py
@@ -2,6 +2,7 @@ import chaste_codegen as cg
 import sympy as sp
 import time
 from chaste_codegen._partial_eval import partial_eval
+from cellmlmanip.model import NumberDummy
 
 
 class CvodeChasteModel(cg.ChasteModel):
@@ -12,15 +13,37 @@ class CvodeChasteModel(cg.ChasteModel):
         super().__init__(model, file_name, **kwargs)
         self._use_analytic_jacobian = kwargs.get('use_analytic_jacobian', False)  # store if jacobians are needed
         self._jacobian_equations, self._jacobian_matrix = self._get_jacobian()
+        self._formatted_jacobian_equations, self._formatted_jacobian_matrix = self._format_jacobian()
 
     def _get_jacobian(self):
         jacobian_equations, jacobian_matrix = [], []
         if self._use_analytic_jacobian:
             state_var_matrix = sp.Matrix(self._state_vars)
+            # get deriv eqs and substitute in all variables other than state vars
             derivative_eqs = partial_eval(self._derivative_equations, self._y_derivatives, keep_multiple_usages=False)
-            derivative_eq_matrix = sp.Matrix([eq.rhs for eq in derivative_eqs])
+            # sort by state var so both are in the same order
+            derivative_eqs = sorted(derivative_eqs, key=lambda d: self._state_vars.index(d.lhs.args[0]))
+            # we're only interested in the rhs
+            derivative_eqs = [eq.rhs for eq in derivative_eqs]
+            # Stick NumberDummies back in and replace _exp by the real exp, so we can do derivatives properly
+            for i in range(len(derivative_eqs)):
+                unit = self._model.units.get_unit('dimensionless')
+                subs_dict = {d: NumberDummy(d, unit) for d in derivative_eqs[i].atoms(sp.Float)}
+                derivative_eqs[i] = derivative_eqs[i].xreplace(subs_dict)
+                derivative_eqs[i] = derivative_eqs[i].subs({sp._exp: sp.exp})
+            derivative_eq_matrix = sp.Matrix(derivative_eqs)
             jacobian_matrix = derivative_eq_matrix.jacobian(state_var_matrix)
+            # Replace exp by _exp again and remove numberDummies so we can safely simplify
+            for i in range(len(jacobian_matrix)):
+                jacobian_matrix[i] = jacobian_matrix[i].subs({sp.exp: sp._exp})
+                subs_dict = {d: sp.Float(d.value, 17) for d in jacobian_matrix[i].atoms(NumberDummy)}
+                jacobian_matrix[i] = jacobian_matrix[i].xreplace(subs_dict)
+            jacobian_equations, jacobian_matrix = sp.cse(jacobian_matrix)
         return jacobian_equations, jacobian_matrix
+
+    def _format_jacobian(self)
+        equations, matr = self._jacobian_equations, self._jacobian_matrix
+        return equations, matr
 
     def generate_chaste_code(self):
         """ Generates and stores chaste code for the CVODE model"""
