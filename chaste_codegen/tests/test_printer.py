10517b3e0e0a96e846431bc6ae537f261f26a844
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2020-02-27 13:30:03 +0000

    update printer

--- chaste_codegen/tests/test_printer.py
+++ chaste_codegen/tests/test_printer.py
@@ -57,22 +57,26 @@ class TestChastePrinter(object):
         assert printer.doprint(conditional_expr) == '((x > 0) ? (0) : ((x > 1) ? (1) : (2)))'
 
     def test_abs(self, printer, x, y):
-        assert printer.doprint(_abs(x + y)) == 'fabs(x + y)'
-        assert printer.doprint(_abs(sp.Float('3.2', 17), evaluate=False)) == 'fabs(3.2000000000000002)'
-        assert printer.doprint(_abs(-3, evaluate=False)) == 'fabs(-3)'
+        assert printer.doprint(sp.Abs(x + y)) == 'fabs(x + y)'
+        assert printer.doprint(sp.Abs(sp.Float('3.2', 17), evaluate=False)) == 'fabs(3.2000000000000002)'
+        assert printer.doprint(sp.Abs(-3, evaluate=False)) == 'fabs(-3)'
 
     def test_trig_functions(self, printer, x):
         # Trig functions
+        assert printer.doprint(sp.acos(x)) == 'acos(x)'
         assert printer.doprint(sp.acosh(x)) == 'acosh(x)'
         assert printer.doprint(sp.asin(x)) == 'asin(x)'
         assert printer.doprint(sp.asinh(x)) == 'asinh(x)'
         assert printer.doprint(sp.atan(x)) == 'atan(x)'
         assert printer.doprint(sp.atanh(x)) == 'atanh(x)'
         assert printer.doprint(sp.ceiling(x)) == 'ceil(x)'
+        assert printer.doprint(sp.cos(x)) == 'cos(x)'
         assert printer.doprint(sp.cosh(x)) == 'cosh(x)'
+        assert printer.doprint(sp.exp(x)) == 'exp(x)'
         assert printer.doprint(sp.factorial(x)) == 'factorial(x)'
         assert printer.doprint(sp.floor(x)) == 'floor(x)'
         assert printer.doprint(sp.log(x)) == 'log(x)'
+        assert printer.doprint(sp.sin(x)) == 'sin(x)'
         assert printer.doprint(sp.sinh(x)) == 'sinh(x)'
         assert printer.doprint(sp.tan(x)) == 'tan(x)'
         assert printer.doprint(sp.tanh(x)) == 'tanh(x)'
