b90ce5412690dd10f33198ba5171b7feb4524689
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2020-04-27 13:23:50 +0100

    parametersied hpp templates

--- chaste_codegen/templates/cvode_with_data_clamp_model.hpp
+++ chaste_codegen/templates/cvode_with_data_clamp_model.hpp
@@ -2,14 +2,5 @@
 {% include "Shared/hpp/header_comments" %}
 {% include "Shared/hpp/includes" %}
 #include "AbstractCvodeCellWithDataClamp.hpp"
-
-class {{class_name}} : public AbstractCvodeCellWithDataClamp{%- if dynamically_loadable %}, public AbstractDynamicallyLoadableEntity{%- endif %}
-{
-    friend class boost::serialization::access;
-    template<class Archive>
-    void serialize(Archive & archive, const unsigned int version)
-    {
-        archive & boost::serialization::base_object<AbstractCvodeCellWithDataClamp >(*this);
-{% include "Shared/hpp/AbstractDynamicallyLoadableEntity" %}
-    }
+{% with %}{% set base_class = "AbstractCvodeCellWithDataClamp" %}{% include "Shared/hpp/class_declaration" %}{% endwith %}
 {% include "Cvode/hpp/public" %}
