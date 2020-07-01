d77a4c95023339dd50872af3e817ebfe3ecf471e
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2020-07-01 11:59:29 +0100

    fix for python versions printing just rhs for rhs (rather than lhs+rhs)

--- chaste_codegen/tests/test_jacobian.py
+++ chaste_codegen/tests/test_jacobian.py
@@ -102,8 +102,8 @@ def test_format_wrong_params5(jacobian):
 
 def test_format_jacobian(jacobian):
     jacobian_equations, jacobian_matrix = jacobian
-    equations, jacobian = format_jacobian(jacobian_equations, sp.Matrix([jacobian_matrix]), ChastePrinter(),
-                                          lambda x, y: str(x) + str(y))
+    equations, jacobian = format_jacobian(jacobian_equations, sp.Matrix([jacobian_matrix]), cg.ChastePrinter(),
+                                          lambda x, y: str(y))
     # order dictionary for printing
     equations = [collections.OrderedDict([('lhs', eq['lhs']), ('rhs', eq['rhs']), ('sympy_lhs', eq['sympy_lhs'])])
                  for eq in equations]
@@ -113,32 +113,7 @@ def test_format_jacobian(jacobian):
     print(equations)
     print(jacobian)
     assert str(equations) == \
-        ("[OrderedDict([('lhs', 'x0'), ('rhs', 'x0120.0*_sodium_channel_m_gate$m**3.0'), ('sympy_lhs', x0)]), OrderedD"
-         "ict([('lhs', 'x1'), ('rhs', 'x1_membrane$V - 40.0'), ('sympy_lhs', x1)]), OrderedDict([('lhs', 'x2'), ('rhs'"
-         ", 'x2exp_(-0.055555555555555556*_membrane$V - 4.1666666666666667)'), ('sympy_lhs', x2)]), OrderedDict([('lhs"
-         "', 'x3'), ('rhs', 'x31.0 - _sodium_channel_m_gate$m'), ('sympy_lhs', x3)]), OrderedDict([('lhs', 'x4'), ('rh"
-         "s', 'x4-0.1*_membrane$V'), ('sympy_lhs', x4)]), OrderedDict([('lhs', 'x5'), ('rhs', 'x5exp_(x4 - 5.0)'), ('s"
-         "ympy_lhs', x5)]), OrderedDict([('lhs', 'x6'), ('rhs', 'x6x5 - 1.0'), ('sympy_lhs', x6)]), OrderedDict([('lhs"
-         "', 'x7'), ('rhs', 'x70.10000000000000001/x6'), ('sympy_lhs', x7)]), OrderedDict([('lhs', 'x8'), ('rhs', 'x8_"
-         "membrane$V + 50.0'), ('sympy_lhs', x8)]), OrderedDict([('lhs', 'x9'), ('rhs', 'x9exp_(-0.05*_membrane$V - 3."
-         "75)'), ('sympy_lhs', x9)]), OrderedDict([('lhs', 'x10'), ('rhs', 'x10exp_(x4 - 4.5)'), ('sympy_lhs', x10)]),"
-         " OrderedDict([('lhs', 'x11'), ('rhs', 'x11x10 + 1.0'), ('sympy_lhs', x11)]), OrderedDict([('lhs', 'x12'), ('"
-         "rhs', 'x121.0 - _potassium_channel_n_gate$n'), ('sympy_lhs', x12)]), OrderedDict([('lhs', 'x13'), ('rhs', 'x"
-         "13exp_(x4 - 6.5)'), ('sympy_lhs', x13)]), OrderedDict([('lhs', 'x14'), ('rhs', 'x14x13 - 1.0'), ('sympy_lhs'"
-         ", x14)]), OrderedDict([('lhs', 'x15'), ('rhs', 'x150.01/x14'), ('sympy_lhs', x15)]), OrderedDict([('lhs', 'x"
-         "16'), ('rhs', 'x16exp_(0.0125*_membrane$V + 0.9375)'), ('sympy_lhs', x16)]), OrderedDict([('lhs', 'x17'), ('"
-         "rhs', 'x17_membrane$V + 65.0'), ('sympy_lhs', x17)])]")
+        ("[OrderedDict([('lhs', 'x0'), ('rhs', '120.0*_sodium_channel_m_gate$m**3.0'), ('sympy_lhs', x0)]), OrderedDict([('lhs', 'x1'), ('rhs', '_membrane$V - 40.0'), ('sympy_lhs', x1)]), OrderedDict([('lhs', 'x2'), ('rhs', 'exp_(-0.055555555555555556*_membrane$V - 4.1666666666666667)'), ('sympy_lhs', x2)]), OrderedDict([('lhs', 'x3'), ('rhs', '1.0 - _sodium_channel_m_gate$m'), ('sympy_lhs', x3)]), OrderedDict([('lhs', 'x4'), ('rhs', '-0.1*_membrane$V'), ('sympy_lhs', x4)]), OrderedDict([('lhs', 'x5'), ('rhs', 'exp_(x4 - 5.0)'), ('sympy_lhs', x5)]), OrderedDict([('lhs', 'x6'), ('rhs', 'x5 - 1.0'), ('sympy_lhs', x6)]), OrderedDict([('lhs', 'x7'), ('rhs', '0.10000000000000001/x6'), ('sympy_lhs', x7)]), OrderedDict([('lhs', 'x8'), ('rhs', '_membrane$V + 50.0'), ('sympy_lhs', x8)]), OrderedDict([('lhs', 'x9'), ('rhs', 'exp_(-0.05*_membrane$V - 3.75)'), ('sympy_lhs', x9)]), OrderedDict([('lhs', 'x10'), ('rhs', 'exp_(x4 - 4.5)'), ('sympy_lhs', x10)]), OrderedDict([('lhs', 'x11'), ('rhs', 'x10 + 1.0'), ('sympy_lhs', x11)]), OrderedDict([('lhs', 'x12'), ('rhs', '1.0 - _potassium_channel_n_gate$n'), ('sympy_lhs', x12)]), OrderedDict([('lhs', 'x13'), ('rhs', 'exp_(x4 - 6.5)'), ('sympy_lhs', x13)]), OrderedDict([('lhs', 'x14'), ('rhs', 'x13 - 1.0'), ('sympy_lhs', x14)]), OrderedDict([('lhs', 'x15'), ('rhs', '0.01/x14'), ('sympy_lhs', x15)]), OrderedDict([('lhs', 'x16'), ('rhs', 'exp_(0.0125*_membrane$V + 0.9375)'), ('sympy_lhs', x16)]), OrderedDict([('lhs', 'x17'), ('rhs', '_membrane$V + 65.0'), ('sympy_lhs', x17)])]")
 
     assert str(jacobian) == \
-        ("[OrderedDict([('i', 0), ('j', 0), ('entry', '-0.29999999999999999 - 36.0 * pow(potassium_channel_n_gate$n, 4"
-         ".0) - x0 * sodium_channel_h_gate$h')]), OrderedDict([('i', 1), ('j', 0), ('entry', '-x3 * x7 + 0.22222222222"
-         "222221 * x2 * sodium_channel_m_gate$m - 0.01 * x3 * x5 * x8 / pow(x6, 2)')]), OrderedDict([('i', 2), ('j', 0"
-         "), ('entry', '-0.050000000000000003 * x9 * (0.070000000000000007 - 0.070000000000000007 * sodium_channel_h_g"
-         "ate$h) - 0.10000000000000001 * x10 * sodium_channel_h_gate$h / pow(x11, 2)')]), OrderedDict([('i', 3), ('j',"
-         " 0), ('entry', '-x12 * x15 - 0.0015625000000000001 * x16 * potassium_channel_n_gate$n - 0.001 * x12 * x13 * "
-         "x17 / pow(x14, 2)')]), OrderedDict([('i', 0), ('j', 1), ('entry', '-360.0 * x1 * pow(sodium_channel_m_gate$m"
-         ", 2.0) * sodium_channel_h_gate$h')]), OrderedDict([('i', 1), ('j', 1), ('entry', '-4.0 * x2 + x7 * x8')]), O"
-         "rderedDict([('i', 0), ('j', 2), ('entry', '-x0 * x1')]), OrderedDict([('i', 2), ('j', 2), ('entry', '-1.0 / "
-         "x11 - 0.070000000000000007 * x9')]), OrderedDict([('i', 0), ('j', 3), ('entry', '-144.0 * pow(potassium_chan"
-         "nel_n_gate$n, 3.0) * (87.0 + membrane$V)')]), OrderedDict([('i', 3), ('j', 3), ('entry', '-0.125 * x16 + x15"
-         " * x17')])]")
+        ("[OrderedDict([('i', 0), ('j', 0), ('entry', '-0.29999999999999999 - 36.0 * pow(potassium_channel_n_gate$n, 4.0) - x0 * sodium_channel_h_gate$h')]), OrderedDict([('i', 1), ('j', 0), ('entry', '-x3 * x7 + 0.22222222222222221 * x2 * sodium_channel_m_gate$m - 0.01 * x3 * x5 * x8 / pow(x6, 2)')]), OrderedDict([('i', 2), ('j', 0), ('entry', '-0.050000000000000003 * x9 * (0.070000000000000007 - 0.070000000000000007 * sodium_channel_h_gate$h) - 0.10000000000000001 * x10 * sodium_channel_h_gate$h / pow(x11, 2)')]), OrderedDict([('i', 3), ('j', 0), ('entry', '-x12 * x15 - 0.0015625000000000001 * x16 * potassium_channel_n_gate$n - 0.001 * x12 * x13 * x17 / pow(x14, 2)')]), OrderedDict([('i', 0), ('j', 1), ('entry', '-360.0 * x1 * pow(sodium_channel_m_gate$m, 2.0) * sodium_channel_h_gate$h')]), OrderedDict([('i', 1), ('j', 1), ('entry', '-4.0 * x2 + x7 * x8')]), OrderedDict([('i', 0), ('j', 2), ('entry', '-x0 * x1')]), OrderedDict([('i', 2), ('j', 2), ('entry', '-1.0 / x11 - 0.070000000000000007 * x9')]), OrderedDict([('i', 0), ('j', 3), ('entry', '-144.0 * pow(potassium_channel_n_gate$n, 3.0) * (87.0 + membrane$V)')]), OrderedDict([('i', 3), ('j', 3), ('entry', '-0.125 * x16 + x15 * x17')])]")
