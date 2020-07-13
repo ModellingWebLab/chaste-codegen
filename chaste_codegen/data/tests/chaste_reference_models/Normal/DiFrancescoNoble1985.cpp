8ac2312db46fe2f70cbfa4425a6218c07297fc0f
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2020-07-13 18:00:52 +0100

    update ref

--- chaste_codegen/data/tests/chaste_reference_models/Normal/DiFrancescoNoble1985.cpp
+++ chaste_codegen/data/tests/chaste_reference_models/Normal/DiFrancescoNoble1985.cpp
@@ -34,7 +34,6 @@
         this->mpSystemInfo = OdeSystemInformation<CellDiFrancescoNoble1985FromCellML>::Instance();
         Init();
         
-        this->mParameters[0] = 0; // (var_membrane__i_pulse) [nanoA]
     }
 
     CellDiFrancescoNoble1985FromCellML::~CellDiFrancescoNoble1985FromCellML()
@@ -297,8 +296,9 @@
             const double var_membrane__C = 0.074999999999999997; // microF
             const double var_hyperpolarising_activated_current__i_f = var_hyperpolarising_activated_current__i_fK + var_hyperpolarising_activated_current__i_fNa; // nanoA
             const double var_membrane__i_pulse_converted = GetIntracellularAreaStimulus(var_chaste_interface__environment__time_converted); // uA_per_cm2
+            const double var_membrane__i_pulse = 1000.0 * var_membrane__i_pulse_converted / HeartConfig::Instance()->GetCapacitance(); // nanoA
             const double var_second_inward_current__i_si = var_second_inward_current__i_siCa + var_second_inward_current__i_siK + var_second_inward_current__i_siNa; // nanoA
-            const double var_membrane__V_orig_deriv = (-var_Na_Ca_exchanger__i_NaCa - var_calcium_background_current__i_Ca_b - var_fast_sodium_current__i_Na - var_hyperpolarising_activated_current__i_f - mParameters[0] - var_second_inward_current__i_si - var_sodium_background_current__i_Na_b - var_sodium_potassium_pump__i_p - var_time_dependent_potassium_current__i_K - var_time_independent_potassium_current__i_K1 - var_transient_outward_current__i_to) / var_membrane__C; // millivolt / second
+            const double var_membrane__V_orig_deriv = (-var_Na_Ca_exchanger__i_NaCa - var_calcium_background_current__i_Ca_b - var_fast_sodium_current__i_Na - var_hyperpolarising_activated_current__i_f - var_membrane__i_pulse - var_second_inward_current__i_si - var_sodium_background_current__i_Na_b - var_sodium_potassium_pump__i_p - var_time_dependent_potassium_current__i_K - var_time_independent_potassium_current__i_K1 - var_transient_outward_current__i_to) / var_membrane__C; // millivolt / second
             d_dt_chaste_interface_var_membrane__V = 0.001 * var_membrane__V_orig_deriv; // millivolt / millisecond
         }
         
@@ -421,10 +421,6 @@ void OdeSystemInformation<CellDiFrancescoNoble1985FromCellML>::Initialise(void)
     this->mVariableNames.push_back("intracellular_potassium_concentration__Ki");
     this->mVariableUnits.push_back("millimolar");
     this->mInitialConditions.push_back(140.0);
-
-    // mParameters[0]:
-    this->mParameterNames.push_back("membrane__i_pulse");
-    this->mParameterUnits.push_back("nanoA");
 
     // Derived Quantity index [0]:
     this->mDerivedQuantityNames.push_back("environment__time");
