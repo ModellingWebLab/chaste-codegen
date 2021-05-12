0192426efe0dcd6ac1e945ce5382be00cac787d1
Maurice Hendrix <maurice.hendrix@nottingham.ac.uk>
2021-05-12 14:52:23 +0100

    typo in template

--- chaste_codegen/data/tests/chaste_reference_models/Normal/output_class.h
+++ chaste_codegen/data/tests/chaste_reference_models/Normal/output_class.h
@@ -35,7 +35,7 @@ class Chaste_CG : public AbstractCardiacCell, public AbstractDynamicallyLoadable
 
 private:
     static AbstractCardiacCell* CreateMethod(boost::shared_ptr<AbstractIvpOdeSolver> p_solver, boost::shared_ptr<AbstractStimulusFunction> p_stimulus);
-    static bool s_registered;
+    static bool registered;
 public:
 
     boost::shared_ptr<RegularStimulus> UseCellMLDefaultStimulus();
